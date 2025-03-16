2f9308ce86bf2711c128128ff71d981ec20e4a07
GokulKumar03 <gokulkumar0307@gmail.com>
2025-03-16 19:52:04 +0530

    first commit

--- 04.variables3.sh
+++ 04.variables3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+$DATE=(date)
 
-echo "$PERSON1: Good Morning Sam"
-echo "$PERSON2: Good Morning"
-echo "$PERSON1: Are you doing well"
-echo "$PERSON2: I am doing well"
+echo "Today's date is $DATE"
+
+
+
+
+
---
+++ 05.variableswith arguments1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Good Morning Sam"
+echo "$PERSON2: Good Morning"
+echo "$PERSON1: Are you doing well"
+echo "$PERSON2: I am doing well"
