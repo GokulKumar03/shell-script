e42035f202aa8639ea2462166cd5c110c83d6131
GokulKumar03 <gokulkumar0307@gmail.com>
2025-03-16 19:39:04 +0530

    first commit

---
+++ 04.variables3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=RAHUL
+PERSON2=SACHIN
+
+echo "$PERSON1: Good Morning Sam"
+echo "$PERSON2: Good Morning"
+echo "$PERSON1: Are you doing well"
+echo "$PERSON2: I am doing well"
