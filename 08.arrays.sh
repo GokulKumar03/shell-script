3b8e09e255a0998c93274d4f443681f9e7d07179
GokulKumar03 <gokulkumar0307@gmail.com>
2025-03-21 09:50:50 +0530

    First commit

--- 08.arrays.sh
+++ 08.arrays.sh
@@ -4,8 +4,8 @@
 
 FRUITS=("Apple" "Mango" "Orange")
 
-echo "The first fruit name is: $FRUITS[0]"
-echo "The second fruit name is: $FRUITS[1]"
-echo "The Third fruit name is: $FRUITS[2]"
+echo "The first fruit name is: ${FRUITS[0]}"
+echo "The second fruit name is: ${FRUITS[1]}"
+echo "The Third fruit name is: ${FRUITS[2]}"
 
-echo "All fruits name is: $FRUITS[@]"
+echo "All fruits name is: ${FRUITS[@]}"
