6c11dd670935d2e7ac73609e0e99e5dcfbf01f3e
GokulKumar03 <gokulkumar0307@gmail.com>
2025-03-21 09:38:37 +0530

    First commit

---
+++ 08.arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# In arrays, Index starts from zero
+
+FRUITS=("Apple" "Mango" "Orange")
+
+echo"The first fruit name is: $FRUITS[0]"
+echo"The second fruit name is: $FRUITS[1]"
+echo"The Third fruit name is: $FRUITS[2]"
+
+echo"All fruits name is: $FRUITS[@]"
